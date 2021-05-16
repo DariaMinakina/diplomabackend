f48655974be312f7da8503cc2515813aff3473cc
dariakrichit <65601694+dariakrichit@users.noreply.github.com>
2021-05-17 00:29:09 +0300

    Initial commit

---
+++ src/test/java/ru/sfedu/diplomabackend/DiplomabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sfedu.diplomabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
